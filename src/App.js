52e205b03075195dce43064225cf7d21b2dcd39b
Lembani <Lembanisakala@gmail.com>
2024-07-30 19:19:44 +0200

    Complete app setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import EventForm from './components/EventForm';
+import EventList from './components/EventList';
+import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+      axios.get('http://localhost:8000/api/events/')
+          .then(response => setEvents(response.data))
+          .catch(error => console.error("There was an error fetching the events!", error));
+  }, []);
+
+  const addEvent = (newEvent) => {
+      setEvents([...events, newEvent]);
+  };
+
+    return (
+        <Container fluid>
+            <Row>
+                <Col xs={12} md={8}>
+                    <h1 className="text-center my-4">Event Scheduler</h1>
+                    <EventList events={events} />
+                </Col>
+                <Col xs={12} md={4} className="my-4">
+                  <EventForm onAddEvent={addEvent} />
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
